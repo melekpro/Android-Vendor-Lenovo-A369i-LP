6e47f4d8ae8d1c56b29f66d7e4108f97fe7b0a2c
Raj Kumar <33460256+Attached2Android@users.noreply.github.com>
2018-07-01 13:50:23 +0530

    Add files via upload

---
+++ A369i-vendor.mk
@@ -0,0 +1,12 @@
+
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A369i/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A369i/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A369i/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A369i/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A369i/proprietary/xbin,system/xbin)
+
